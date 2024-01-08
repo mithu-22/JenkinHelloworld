9fbe4b991f7b0761c07394fad53e2a580ab83797
mithu-22 <mithraraj1912@gmail.com>
2024-01-08 15:39:28 +0530

    #1 change in webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Worlds!");      
         System.out.println("This is First line.");
-        System.out.println("This is another line.");  
+        System.out.println("This is second line.");  
     }
 }
