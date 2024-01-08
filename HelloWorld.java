715a684c05f80c7d84b3721e6cf3fb5a4f067486
mithu-22 <mithraraj1912@gmail.com>
2024-01-08 15:04:07 +0530

    #2 change in poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Worlds!");        
+        System.out.println("Hello, Worlds!");      
+        System.out.println("This is First line.");
+        System.out.println("This is another line.");  
     }
 }
